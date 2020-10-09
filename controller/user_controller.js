e1d31c5a0bcc3cfdd22e3ea6f2722396ab17a142
Shih-Pnag <leebond88@yahoo.com.tw>
2020-10-09 20:39:54 +0800

    add

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -3,11 +3,22 @@ const { ForbiddenError } = require('apollo-server')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { v4: uuidv4 } = require('uuid')
+const nodemailer = require('nodemailer')
+const smtpTransport = require('nodemailer-smtp-transport')
 
 const isAuthenticated = resolverFunction => (parent, args, context) => {
     if (!context.me) return new ForbiddenError('Not logged in.')
     return resolverFunction.apply(null, [parent, args, context])
 }
+
+let transporter = nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    auth: {
+         user: process.env.USERMAIL,
+         pass: process.env.USERPASS
+    }
+}))
 
 const UserController = {
     getAll: async () => await UserModel.find({}),
@@ -32,6 +43,22 @@ const UserController = {
             if(user.length == 0) return new ForbiddenError('Email Not Found')
             let passwordCompare = await bcrypt.compare(input.password, user[0].password)
             if(!passwordCompare) return new ForbiddenError('Password Not Same')
+
+            const mailOptions = {
+                from: 'leebond88@yahoo.com.tw',
+                to: input.email,
+                subject: 'Confrimation Email',
+                html: '<h1>Hello</h1>'
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log(error);  
+                } else {     
+                    console.log('Email sent: ' + info.response);  
+                }   
+           })
+
             let sign = { token: jwt.sign({ uid: user[0] }, process.env.SECRET, { expiresIn: '1d' }) }
             return Object.assign(user[0], sign)
         } catch(e) {
